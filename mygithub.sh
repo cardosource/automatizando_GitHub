10670e7c20dfbc547fdea28404d45cbeb2139214
jeferson cardoso <jefersoncardoso.dev@gmail.com>
2021-09-02 22:07:41 -0400

    Update mygithub.sh

--- mygithub.sh
+++ mygithub.sh
@@ -40,3 +40,47 @@ license(){
    esac
    
    }
+
+function iniciandoTrabalhos(){
+   echo -e "\n"
+  echo -e "Deseja usar alguma licença de uso de software : "
+  read -p "[ LICENSE    ] - Adicionar LICENSE sim/nao  " adicionarlicense  
+  if [[ $adicionarlicense == "sim" ]];
+      then
+         echo -e  "1 - MIT\n2 - GPL v3\n3 - Apache 2.0"
+         read  license
+          license $license 
+  fi
+  
+  if [[ $gitignore == *"gitignore"  ]];
+  then
+  
+   echo -e "\n-Extenções ignoradas. \n "
+   cat .gitignore
+   
+   echo -e "----------------------\n"
+   
+   
+   else
+     echo -e "\n-Não foi encontrado  um .gitignore  não deseja ser adicionada.\n
+              lembro que ao "
+     read -p "[ GITIGNORE  ] sim ou nao " adicionargitignore
+     if [[ $adicionargitignore == "sim" ]];
+      then
+          echo "criado"
+          touch .gitignore
+          nano .gitignore
+      fi
+   
+   fi
+  for  indice in  $arquivos
+  do
+   adicionarArquivo "$indice"  
+   #adicionarArquivo primeira chamada
+   echo "[ ADICIONADO ] - Arquivo :  $indice"
+   
+   
+
+  done
+         
+ }
